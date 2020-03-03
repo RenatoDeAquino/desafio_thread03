39162951bea78fae5281943822b0684ad01ac36d
unknown <17070024@acad.cesupa>
2020-03-03 14:25:32 -0300

    logica da função do botão mudar de lugar faltando somente a thread no timer de acabar o jogo e a thread de mudar o botão de lugar automaticamente

--- desafio_thread03.py
+++ desafio_thread03.py
@@ -1,28 +1,37 @@
-from threading import Timer
 from tkinter import *
+from threading import *
 import random
+posx = random.randint(0,400)
+posy = random.randint(0,400)
+x = 0
 
-def setInterval(function, interval, *params, **kwparams):
-    def setTimer(wrapper):
-        wrapper.timer = Timer(interval, wrapper)
-        wrapper.timer.start()
 
-    def wrapper():
-        function(*params, **kwparams)
-        setTimer(wrapper)
-    
-    setTimer(wrapper)
-    return wrapper
-def clearInterval(wrapper):
-    wrapper.timer.cancel()
-    
+def aoClicar():
+    global pontos
+    pontos.destroy()
+    posx = random.randint(0,400)
+    posy = random.randint(0,400)
+    botao.pack()
+    botao.place(bordermode = OUTSIDE,heigh = 70,width = 70,x = posx,y = posy)
+    global x
+    x += 1
+    pontos = Label(window,text = "seu total de pontos é: "+str(x), font ="impact 15 bold")
+    pontos.pack(side = "bottom")
 
-class Application:
-    def __init__(self, master=None):
-    
-          l = Label(master, text = "u dont pega me")
-          l.place(relx = 1, rely = 1)
-root = Tk()
-root.geometry('500x500')
-Application(root)
-root.mainloop()
+window = Tk()
+#config da window
+window.geometry("500x500+200+100")
+window.title("TI fora da caixa")
+#label mensagem
+mensagem = Label(window, text="Aperte o botão", font="impact 15 bold")
+mensagem.pack()
+mensagem.place(x= 0, y =0)
+#label pontos
+pontos = Label(window,text = "seu total de pontos é: "+str(x), font ="impact 15 bold")
+pontos.pack(side = "bottom")
+#botao de ação
+botao = Button(window, text="Clique Aqui", command=aoClicar)
+botao.pack()
+botao.place(bordermode = OUTSIDE,heigh = 70,width = 70,x = posx,y = posy)
+#executa o tkinter
+window.mainloop()
